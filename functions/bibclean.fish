0fe80bd814a1b3421a92d5612af9bb216b7f9c1b
Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>
2018-10-07 02:43:11 +0900

    Download python file

--- functions/bibclean.fish
+++ functions/bibclean.fish
@@ -1,3 +1,6 @@
 function bibclean
+    if not test -e ~/.config/fish/functions/extract_used_bib_entries.py
+        wget https://raw.githubusercontent.com/nzw0301/my-fish/master/functions/extract_used_bib_entries.py
+    end
     python ~/.config/fish/functions/extract_used_bib_entries.py $argv;
 end
