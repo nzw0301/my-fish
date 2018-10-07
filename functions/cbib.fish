3d046d2edabd4120b13ecab7825bc7a251f5a40c
Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>
2018-10-07 17:42:52 +0900

    Rename

--- functions/cbib.fish
+++ functions/cbib.fish
@@ -1,4 +1,4 @@
-function bibclean
+function cbib
     if not test -e ~/.config/fish/functions/extract_used_bib_entries.py
         wget https://raw.githubusercontent.com/nzw0301/my-fish/master/functions/extract_used_bib_entries.py
     end
