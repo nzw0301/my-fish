d5c995978b02d20d62637c9149e7efe7e2689e66
Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>
2018-10-07 21:01:47 +0900

    Fix save path

--- functions/cbib.fish
+++ functions/cbib.fish
@@ -1,6 +1,6 @@
 function cbib
     if not test -e ~/.config/fish/functions/extract_used_bib_entries.py
-        wget https://raw.githubusercontent.com/nzw0301/my-fish/master/functions/extract_used_bib_entries.py
+        wget https://raw.githubusercontent.com/nzw0301/my-fish/master/functions/extract_used_bib_entries.py -P ~/.config/fish/functions
     end
     python ~/.config/fish/functions/extract_used_bib_entries.py $argv;
 end
