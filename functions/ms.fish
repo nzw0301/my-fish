22a8bef7a9fe98bf9ecc97f1354e071205164260
Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>
2019-02-09 18:13:50 +0900

    fix typo

--- functions/ms.fish
+++ functions/ms.fish
@@ -1,4 +1,4 @@
 function ms
-    ssh kserver2 /mslab/link/m $argv;
-    ssh server2 /home/local/bin/ms-script/ms $argv;
+    ssh kserver2 /mslab/link/ms $argv
+    ssh server2 /home/local/bin/ms-script/ms $argv
 end
