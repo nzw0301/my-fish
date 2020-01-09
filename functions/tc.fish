6e37c9ac1a7d0c97de0f4b40b5a31915bcf4f0b5
NOZAWA Kento <k_nzw@klis.tsukuba.ac.jp>
2020-01-09 11:50:56 +0900

    Add remove unused files and dirs command

---
+++ functions/tc.fish
@@ -0,0 +1,10 @@
+function tc
+    set data "*.log" "*.xdv" "*.aux" "main.out" "*.bbl" "*.blg" "*.fdb_latexmk" "*.fls" "*.dvi" ".ipynb_checkpoints" "auto" "__pycache__" "*.egg-info" "dist" "build" "*.pyc" "*.cpython-35m-darwin.so" ".pytest_cache*" "*\[Conflict\]" "*.synctex.gz"
+    for val in $data
+        if string match $argv "d" > /dev/null ;
+            find . -name $val -exec rm -r "{}" \;
+        else
+            find . -name $val;
+        end
+    end
+end
