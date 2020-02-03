ff3e33f634771f1229f990ebe131f1cdb37b7b22
NOZAWA Kento <k_nzw@klis.tsukuba.ac.jp>
2020-02-03 17:13:51 +0900

    Add wild card to the end of conflict

--- functions/tc.fish
+++ functions/tc.fish
@@ -1,5 +1,5 @@
 function tc
-    set data "*.log" "*.xdv" "*.aux" "main.out" "*.bbl" "*.blg" "*.fdb_latexmk" "*.fls" "*.dvi" ".ipynb_checkpoints" "auto" "__pycache__" "*.egg-info" "dist" "build" "*.pyc" "*.cpython-35m-darwin.so" ".pytest_cache*" "*\[Conflict\]" "*.synctex.gz" "*.spl" "*.toc"
+    set data "*.log" "*.xdv" "*.aux" "main.out" "*.bbl" "*.blg" "*.fdb_latexmk" "*.fls" "*.dvi" ".ipynb_checkpoints" "auto" "__pycache__" "*.egg-info" "dist" "build" "*.pyc" "*.cpython-35m-darwin.so" ".pytest_cache*" "*\[Conflict\]*" "*.synctex.gz" "*.spl" "*.toc"
     for val in $data
         if string match $argv "d" > /dev/null ;
             find . -name $val -exec rm -r "{}" \;
