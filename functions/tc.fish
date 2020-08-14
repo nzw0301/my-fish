d07e9ec1107cc0825f22641caf146abc33700234
Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>
2020-08-14 09:25:59 +0900

    Update script

--- functions/tc.fish
+++ functions/tc.fish
@@ -1,5 +1,5 @@
 function tc
-    set data "*.log" "*.xdv" "*.aux" "main.out" "*.bbl" "*.blg" "*.fdb_latexmk" "*.fls" "*.dvi" ".ipynb_checkpoints" "auto" "__pycache__" "*.egg-info" "dist" "build" "*.pyc" "*.cpython-35m-darwin.so" ".pytest_cache*" "*\[Conflict\]*" "*.synctex.gz" "*.spl" "*.toc" "*.bcf"
+    set data "*.log" "*.xdv" "*.aux" "main.out" "*.bbl" "*.blg" "*.fdb_latexmk" "*.fls" "*.dvi" ".ipynb_checkpoints" "auto" "__pycache__" "*.egg-info" "dist" "build" "*.pyc" "*.cpython-35m-darwin.so" ".pytest_cache*" "*\[Conflict*" "*.synctex.gz" "*.spl" "*.toc" "*.bcf"
     for val in $data
         if string match $argv "d" > /dev/null ;
             find . -name $val -exec rm -r "{}" \;
